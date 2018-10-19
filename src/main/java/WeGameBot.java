35f525a7b8045fff2c850a9327572bc638d3b672
UnfamousThomas <thompalts@gmail.com>
2018-10-20 00:25:10 +0300

    attempt fixesx

--- src/main/java/WeGameBot.java
+++ src/main/java/WeGameBot.java
@@ -1,13 +1,18 @@
-import commands.help;
+import events.help;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 
+
 public class WeGameBot {
-    public static void main(String[] args) throws Exception {
-        JDA jda = new JDABuilder(AccountType.BOT)
-                .setToken("NDk4NTc3ODM1ODA0MzI3OTcy.Dp_MSQ.gK-2LnlRja6CU0FF1jKp92Ku5Z0")
-                .addEventListener(new help())
-                .build();
+    public static void main(String[] args) {
+        try {
+            JDA jda = new JDABuilder(AccountType.BOT)
+                    .setToken("NDk4NTc3ODM1ODA0MzI3OTcy.Dp_MSQ.gK-2LnlRja6CU0FF1jKp92Ku5Z0")
+                    .addEventListener(new help())
+                    .build();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/events/help.java
@@ -0,0 +1,21 @@
+package events;
+
+import net.dv8tion.jda.core.entities.*;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class help extends ListenerAdapter {
+    @Override
+    public void onGuildMessageReceived(GuildMessageReceivedEvent e) {
+        User author = e.getAuthor();
+        String name = author.getName();
+        TextChannel channel = e.getChannel();
+        Message msg = e.getMessage();
+        boolean bot = author.isBot();
+
+        if (msg.isFromType(ChannelType.TEXT) && !bot) {
+            channel.sendMessage("Hello!");
+        }
+
+        }
+    }
