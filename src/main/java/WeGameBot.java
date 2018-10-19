e478c328c45f9b58bbcea51f93b1e966668671b9
UnfamousThomas <thompalts@gmail.com>
2018-10-19 21:37:06 +0300

    add and remove try to make some stuff work but fail lol

--- src/main/java/WeGameBot.java
+++ src/main/java/WeGameBot.java
@@ -1,14 +1,13 @@
-import events.SendMessageonMessage;
+import commands.help;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
-import utils.Commands;
 
 public class WeGameBot {
     public static void main(String[] args) throws Exception {
         JDA jda = new JDABuilder(AccountType.BOT)
                 .setToken("NDk4NTc3ODM1ODA0MzI3OTcy.Dp_MSQ.gK-2LnlRja6CU0FF1jKp92Ku5Z0")
-                .addEventListener(new Commands())
+                .addEventListener(new help())
                 .build();
     }
 }
--- src/main/java/commands/help.java
+++ src/main/java/commands/help.java
@@ -1,9 +1,12 @@
 package commands;
 
 import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
-public class help {
-    public static void run(Message msg) {
-        msg.getChannel().sendMessage("Helllloooo!").queue();
-    }
-}
+public class help extends ListenerAdapter {
+    public void onMessage(MessageReceivedEvent e) {
+        if(!(e.getAuthor().isBot()) && e.getMessage().getContentRaw().equalsIgnoreCase("!help")) {
+            e.getChannel().sendMessage("helloooooooooo!").queue();
+        }
+    }}
