aff9aeb9bfc357310aaa6feb4a73f757c067ffa9
UnfamousThomas <thompalts@gmail.com>
2018-10-20 13:03:16 +0300

    FIIIIIIIIIIIIIIIIIIIIIIIIIIXED IT WOOO

--- src/main/java/WeGameBot.java
+++ src/main/java/WeGameBot.java
@@ -1,4 +1,4 @@
-import events.onMessageReceived;
+import events.pingCommand;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
@@ -9,7 +9,7 @@ public class WeGameBot {
         try {
             JDA jda = new JDABuilder(AccountType.BOT)
                     .setToken("NDk4NTc3ODM1ODA0MzI3OTcy.Dp_MSQ.gK-2LnlRja6CU0FF1jKp92Ku5Z0")
-                    .addEventListener(new onMessageReceived())
+                    .addEventListener(new pingCommand())
                     .build();
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/java/events/pingCommand.java
+++ src/main/java/events/pingCommand.java
@@ -4,7 +4,7 @@ import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
-public class onMessageReceived extends ListenerAdapter {
+public class pingCommand extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent e) {
         User author = e.getAuthor();
@@ -13,8 +13,8 @@ public class onMessageReceived extends ListenerAdapter {
         Message msg = e.getMessage();
 
         if (author.isBot()) { return; }
-        else if(msg.equals("!helo")) {
-            channel.sendMessage("HEY THERE, " + name);
+        else if(msg.getContentRaw().equalsIgnoreCase("!ping")) {
+            channel.sendMessage(author.getAsMention() + " Pong!").queue();
         }
 
         }
