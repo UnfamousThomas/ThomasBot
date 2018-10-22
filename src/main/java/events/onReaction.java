cfd5d47211f1d3655e44fa325a55d0fb59783888
UnfamousThomas <thompalts@gmail.com>
2018-10-22 17:05:12 +0300

    try and fail

--- src/main/java/events/onReaction.java
+++ src/main/java/events/onReaction.java
@@ -6,7 +6,7 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
 public class onReaction extends ListenerAdapter {
     @Override
     public void onMessageReactionAdd(MessageReactionAddEvent e) {
-        if(e.getReactionEmote().getName().equals("\u1F600")) {
+        if(e.getReactionEmote().getName().equals("\u1f600")) {
             e.getChannel().sendMessage("GRINNING!!!!").queue();
         }
     }
